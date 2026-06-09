0769b87e13eea214cd5abac5507be4a49ff36245
YeRudIT <rudencoyehoward@gmail.com>
2026-06-09 14:34:48 +0300

    Rezolvarea validării proaste a unor întrebări de practică.

--- src/content/cestionare/atb-cdos-2026.js
+++ src/content/cestionare/atb-cdos-2026.js
@@ -534,7 +534,7 @@ export default {
                             "Studenții au dreptul de a beneficia de acoperirea costurilor necesare pentru efectuarea practicii comasate, pentru perioada prevăzută în planurile de învătământ, inclusiv cheltuieli de masă, cazare și transport, in situațiile in care practica se desfășoara in afara centrului universitar respectiv (institutului), conform prevederilor art. 128 alin. (25) din Legea nr. 199/2023, cu modificările și completarile ulterioare;",
 
                         ascunde: (rspi) =>
-                            rspi["practica-de-specialitate"] !== "în proces",
+                            rspi["practica-de-specialitate"] == "în proces",
                     },
                 ),
                 da_nu_caz(
@@ -544,7 +544,7 @@ export default {
                         desc:
                             "Studenții pot beneficia de cazare în căminele universitare pe perioada practicii desfășurate în cadrul universității",
                         ascunde: (rspi) =>
-                            rspi["practica-de-specialitate"] !== "în proces",
+                            rspi["practica-de-specialitate"] == "în proces",
                     },
                 ),
                 da_nustiu(
@@ -556,7 +556,7 @@ export default {
                     "Ai avut posibilitatea de a evalua calitatea stagiului de practică și de a sesiza eventuale deficiențe? [art. 9 (1) f)]",
                     {
                         ascunde: (rspi) =>
-                            rspi["practica-de-specialitate"] !== "în proces",
+                            rspi["practica-de-specialitate"] == "în proces",
                     },
                 ),
                 da_nu_caz(
@@ -564,7 +564,7 @@ export default {
                     "Ti-a fost recunoscută practica efectuată individual după evaluarea îndeplinirii obiectivelor? [art. 9 (1) g)]",
                     {
                         ascunde: (rspi) =>
-                            rspi["practica-de-specialitate"] !== "în proces",
+                            rspi["practica-de-specialitate"] == "în proces",
                     },
                 ),
                 detalii("detalii-practica"),
